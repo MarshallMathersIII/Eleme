c5393f20f0c50817772fcc1ec539d3c11b74d027
eminem <ghy1060627080@163.com>
2019-07-01 15:58:40 +0800

    header-axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
   </div>
 </template>
 <script>
-  import VHeader from './components/v-header/v-header';
+import VHeader from "./components/v-header/v-header";
+import {getSeller} from 'api'
+
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  methods: {
+    _getSeller(){
+      getSeller().then((seller) =>{
+      this.seller=seller
+    })
+    }
+  },
+  created() {
+    this._getSeller()
+  },
   components: {
     VHeader
   }
-}
+};
 </script>
-<style lang="stylus">
-</style>
+<style lang="stylus"></style>
